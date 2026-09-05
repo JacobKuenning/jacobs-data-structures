7f9b3f6e224e0d644d42e4e3fd06d56bd7f63ecb
JacobKuenning <jacob.kuenning@gmail.com>
2026-09-04 20:29:59 -0700

    Finished llist implementation and testing

--- llist.c
+++ llist.c
@@ -3,29 +3,33 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "llist.h"
 
 llist* create_llist(){
     llist* list = (llist*)malloc(sizeof(llist));
+    if (!list) return NULL;
     list->head = NULL;
     list->tail = NULL;
     list->size = 0;
     return list;
 }
     
-void delete_llist(llist* list){
-    llist_n* ptr = list->tail;
-    llist_n* hd = list->head;
-    while (ptr != hd){ 
-        ptr->next = NULL; // unlink node's next field 
-        ptr = ptr->prev; 
-        ptr->next->prev = NULL; // unlink node's previous field
+void delete_llist(llist* list, void (*llist_n_removal) (llist_n* node)){
+    llist_n* ptr = list->head;
+    while (ptr){
+        llist_n* next = ptr->next;
+        if (llist_n_removal) // if a node removal function is passed, call it
+            llist_n_removal(ptr);
+        else 
+            llist_n_clean(ptr); // otherwise, unlink the node
+        ptr = next;
     }
-    ptr->next = NULL;
     free(list);
 }
 
 void llist_n_clean(llist_n* node){
+    if (!node) return;
     node->next = NULL;
     node->prev = NULL;
 }
@@ -55,10 +59,12 @@ void llist_prepend(llist* list, llist_n* node){
         list->head->prev = node;
         list->head = node;
     }
+    list->size++;
 }
 
 llist_n* llist_get(llist* list, int n){
-    if (n > list->size || n < 0) return NULL;
+    if (!list) return NULL;
+    if (n >= list->size || n < 0) return NULL;
 
     llist_n* ptr = list->head;
     int i = 0;
@@ -72,4 +78,175 @@ llist_n* llist_get(llist* list, int n){
 int llist_size(llist* list){
     if (list) return list->size;
     return 0;
+}
+
+void llist_insert(llist* list, llist_n* node, int n){
+    if (!list || !node) return;
+    llist_n_clean(node);
+    if (n >= list->size){
+        llist_append(list, node);
+        return;
+    }
+    if (n <= 0){
+        llist_prepend(list, node);
+        return;
+    }
+    llist_n* ptr = llist_get(list, n);
+    node->next = ptr;
+    node->prev = ptr->prev;
+    node->prev->next = node;
+    ptr->prev = node;
+    list->size++;
+    return;
+}
+
+void llist_remove(llist* list, int n, void (*llist_n_removal) (llist_n* node)){
+    if (!list) return;
+    llist_n* ptr = llist_get(list, n);
+    if (!ptr) return;
+
+    if (ptr->prev) ptr->prev->next = ptr->next;
+    else list->head = ptr->next;
+
+    if (ptr->next) ptr->next->prev = ptr->prev;
+    else list->tail = ptr->prev;
+
+    if (llist_n_removal) llist_n_removal(ptr);
+    else llist_n_clean(ptr); 
+
+    list->size--;
+    return;
+}
+
+void llist_replace(llist* list, llist_n* node, int n, void (*llist_n_removal) (llist_n* node)){
+    if (!list) return;
+    llist_n* ptr = llist_get(list, n);
+    if (!ptr) return;
+    llist_n_clean(node);
+
+    node->next = ptr->next;
+    node->prev = ptr->prev;
+
+    if (node->next) node->next->prev = node;
+    else list->tail = node;
+
+    if (node->prev) node->prev->next = node;
+    else list->head = node;
+
+    if (llist_n_removal)
+        llist_n_removal(ptr);
+    else
+        llist_n_clean(ptr); 
+    return;
+}
+
+void llist_swap(llist* list, int n, int m){
+    if (!list || n == m) return;
+    if (m < n){ // ensure node1 is always the first
+        int t = m;
+        m = n;
+        n = t;
+    }
+    
+    llist_n* node1 = llist_get(list, n);
+    llist_n* node2 = llist_get(list, m);
+    if (!node1 || !node2) return;
+
+    llist_n* n1_prev = node1->prev;
+    llist_n* n1_next = node1->next;
+    llist_n* n2_prev = node2->prev;
+    llist_n* n2_next = node2->next;
+
+    if (m - n == 1){ // if they are right next to eachother
+        node1->next = n2_next;
+        node1->prev = node2;
+        node2->next = node1;
+        node2->prev = n1_prev;
+    } else {
+        node1->next = n2_next;
+        node1->prev = n2_prev;
+        node2->next = n1_next;
+        node2->prev = n1_prev;
+    }
+
+    if (node1->next) node1->next->prev = node1;
+    else list->tail = node1;
+
+    if (node1->prev) node1->prev->next = node1;
+    else list->head = node1;
+
+    if (node2->next) node2->next->prev = node2;
+    else list->tail = node2;
+
+    if (node2->prev) node2->prev->next = node2;
+    else list->head = node2;
+
+    return;
+}
+
+llist* llist_split(llist* list, int n){
+    if (!list) return NULL;
+    llist_n* ptr = llist_get(list, n);
+    if (!ptr) return (NULL);
+    llist* new = create_llist();
+    if (!new) return (NULL);
+
+    new->size = list->size - n;
+    new->head = ptr;
+    new->tail = list->tail;
+
+    list->tail = ptr->prev;
+    if (list->tail)
+        list->tail->next = NULL;
+    else // list is now empty
+        list->head = NULL;
+
+    new->head->prev = NULL; // sever last connection
+
+    list->size = n;
+    return new;
+}
+
+void llist_concatenate(llist* aug, llist* add){
+    if (!aug || !add) return;
+    if (add->size == 0) return;
+    if (aug->size == 0){
+        aug->head = add->head;
+        aug->tail = add->tail;
+        aug->size = add->size;
+    } else {
+       aug->tail->next = add->head;
+       add->head->prev = aug->tail;
+       aug->tail = add->tail;
+       aug->size += add->size;
+    }
+    add->head = NULL;
+    add->tail = NULL;
+    add->size = 0;
+    return;
+}
+
+void llist_rotate(llist* list, int n){
+    if (!list) return;
+    if (list->size == 0 || list->size == 1) return;
+
+    n = n % list->size;
+    if (n < 0) {
+        n += list->size;
+    }
+    if (n == 0) return;
+
+    list->head->prev = list->tail;
+    list->tail->next = list->head;
+    llist_n* ptr = list->head;
+
+    for (int i = 0; i < n; i ++) {
+       ptr = ptr->next; 
+    }
+    
+    list->tail = ptr->prev;
+    list->tail->next = NULL;
+    list->head = ptr;
+    list->head->prev = NULL;
+    return;
 }
--- llist.h
+++ llist.h
@@ -20,9 +20,45 @@ struct llist{
     int size;
 };
 
+// create a new llist
 llist* create_llist();
-void delete_llist(llist* list);
+
+// unlink all nodes, pass in removal function if data needs to be altered / freed when removed
+void delete_llist(llist* list, void (*llist_n_removal) (llist_n* node));
+
+// helper function to remove links in nodes
 void llist_n_clean(llist_n* node);
+
+// append llist_n to llist
 void llist_append(llist* list, llist_n* node);
+
+// prepend llist_n to llist
+void llist_prepend(llist* list, llist_n* node);
+
+// get a pointer to the llist_n in the nth position
 llist_n* llist_get(llist* list, int n);
+
+// size of llist; # of llist_n in llist
 int llist_size(llist* list);
+
+// insert into llist a llist_n at nth position
+void llist_insert(llist* list, llist_n* node, int n);
+
+// remove llist_n at nth position in llist, pass in removal function if data needs to be altered / freed when removed
+void llist_remove(llist* list, int n, void (*llist_n_removal) (llist_n* node));
+
+// replace llist_n at nth position in llist with another, pass in removal function if data needs to be altered / freed when removed
+void llist_replace(llist* list, llist_n* node, int n, void (*llist_n_removal) (llist_n* node));
+
+// swap the llist_n in the nth and mth positions in the llist
+void llist_swap(llist* list, int n, int m);
+
+// split llist into two, the second starting at the nth position in the original
+llist* llist_split(llist* list, int n);
+
+// append add to aug, afterwards add will be empty
+void llist_concatenate(llist* aug, llist* add);
+
+// rotate llist by n nodes
+void llist_rotate(llist* list, int n);
+
